7efc43568b31799561eefc0b9bfaa84d8f0455e3
SquidicusJohnson <tristanballman@gmail.com>
2023-07-26 15:53:27 -0500

    add haddock notation

--- Permute.hs
+++ Permute.hs
@@ -14,8 +14,6 @@ module Permutation (
   , toFactoriadic
   , fromFactoriadic
   , toTranspositions
-  , fromTranspositions
-  , fromReverseTranspositions
   , fromCycles
   , toCycles
   , imageOfIndex
@@ -26,8 +24,8 @@ import Data.List (unfoldr)
 import qualified Data.Map as M
 import Data.Map ((!))
 
--- Wraps an Integer and represents an abstract permutation.
--- Particulary every positive integer represents a finite set of transpositions.
+-- | Wraps an Integer and represents an abstract permutation.
+--   Particulary every positive integer represents a finite set of transpositions.
 data Permutation = Permutation Integer deriving (Eq, Show)
 
 unwrap (Permutation n) = n
@@ -64,67 +62,68 @@ instance Monoid Permutation where
 -- it is treated as a stack such that the first transposition in the list
 -- occurs before those beneath it.
 
--- Get the inverse of a permutation p
+-- | Get the inverse permutation of a permutation p.
 inverse :: Permutation -> Permutation
 inverse = fromReverseTranspositions . toTranspositions
 
--- The largest index moved by a permutation
+-- | The largest index moved by a permutation.
 size :: Permutation -> Int
 size = (+1) . length . toFactoriadic
 
--- Returns false for a Permutation of even parity, true for odd
+-- | Returns false for permutations with an even number of transpositions,
+--   true for odd.
 parity :: Permutation -> Bool
 parity = odd . length . filter ( /= 0 ) . toFactoriadic
 
--- The number of times you would have to compose a permutation p with itself
--- to get the identity permutation.
+-- | The number of times you would have to compose a permutation p with itself
+--   to get the identity permutation.
 period :: Permutation -> Int
 period = foldr (lcm . length) 1 . toCycles
 
--- compose g h computes the permutation equivalent to permuting a set by h then
--- permuting it by g. Use (<>) for infix
+-- | compose g h computes the permutation equivalent to permuting a set by h then
+--   permuting it by g. Use (<>) for infix.
 compose :: Permutation -> Permutation -> Permutation
 compose (Permutation 0) h = h
 compose g (Permutation 0) = g
 compose g h = fromReverseTranspositions $ foldl (flip leftMultiplyTransposition) (reverse $ toTranspositions g) (toTranspositions h)
 
 
--- Permute a list by a permutation, returns Nothing if the list is
--- shorter than the size of the permutation
+-- | Permute a list by a permutation, returns Nothing if the list is
+--   shorter than the size of the permutation.
 actOnList :: Permutation -> [a] -> Maybe [a]
 actOnList p xs = if length xs < size p
     then Nothing
     else Just $ map (\i -> xs!!i) ( take (length xs) (toIndices' p) )
 
--- Permute a list by the inverse of a permutation, returns Nothing if
--- the list is shorter than the size of the permutation
+-- | Permute a list by the inverse of a permutation, returns Nothing if
+--   the list is shorter than the size of the permutation.
 inverseActOnList :: Permutation -> [a] -> Maybe [a]
 inverseActOnList p xs = if length xs < size p
     then Nothing
     else Just $ map (\i -> xs!!i) ( take (length xs) (toInverseIndices' p))
 
 
--- Converts a permutation p to the result of it acting on the list [0 .. size p - 1]
+-- | Converts a permutation p to the result of it acting on the list [0 .. size p - 1].
 toIndices :: Permutation -> [Int]
 toIndices p = map ( subtract 1 . preimageOfIndex p ) [1 .. size p]
 
--- Converts a permutation p to the result of it acting on the list [0..]
--- The result is an infinite list
+-- | Converts a permutation p to the result of it acting on the list [0..].
+--   The result is an infinite list
 toIndices' :: Permutation -> [Int]
 toIndices' p = toIndices p ++ drop (size p) [0..]
 
--- Converts a permutation p to the result of its inverse acting on the list [0 .. size p - 1]
+-- | Converts a permutation p to the result of its inverse acting on the list [0 .. size p - 1].
 toInverseIndices :: Permutation -> [Int]
 toInverseIndices p = map ( subtract 1 . imageOfIndex p ) [1 .. size p]
 
--- Converts a permutation p to the result of its inverse acting on the list [0..]
--- The result is an infinite list
+-- | Converts a permutation p to the result of its inverse acting on the list [0..].
+--   The result is an infinite list.
 toInverseIndices' :: Permutation -> [Int]
 toInverseIndices' p = toInverseIndices p ++ drop (size p) [0..]
 
 
 
--- Converts a Permutations stored integer into a left to right list of its digits in factoriadic.
+-- | Converts a Permutations stored integer into a left to right list of its digits in factoriadic.
 toFactoriadic :: Permutation -> [Int]
 toFactoriadic = unfoldr (\(n, place) ->
                   if n < 1
@@ -133,21 +132,17 @@ toFactoriadic = unfoldr (\(n, place) ->
               . (\n -> (n, 2) )
               . unwrap
 
--- Converts a left to right list of factoriadic digits to the corresponding Integer
+-- | Converts a left to right list of factoriadic digits to the corresponding Integer
 fromFactoriadic :: [Int] -> Integer
 fromFactoriadic = unwrap . fromTranspositions . zip [2..]
 
--- Converts a permutation to a left to right list of transpositions it represents.
+-- | Converts a permutation to a left to right list of transpositions it represents.
 toTranspositions :: Permutation -> [(Int, Int)]
 toTranspositions = zip [2..] . toFactoriadic
 
-fromTranspositions :: [(Int, Int)] -> Permutation
-fromTranspositions = Permutation . foldr (\(i,j) n -> (n + toInteger j) * toInteger (i-1) ) 0
-
-fromReverseTranspositions :: [(Int,Int)] -> Permutation
-fromReverseTranspositions = Permutation . foldl (\n (i,j) -> (n + toInteger j) * toInteger (i-1) ) 0
-
--- Convert a list of cycles to the corresponding Permutation
+-- | Convert a list of cycles to the corresponding Permutation.
+--   Individual cycles must not contain duplicate indices, but cycles do not
+--   need to be disjoint.
 fromCycles :: [[Int]] -> Permutation
 fromCycles = reduceTranspositions
            . concatMap (
@@ -156,11 +151,11 @@ fromCycles = reduceTranspositions
            )
            . filter (not . null)
 
--- Convert a permutation to cycle notation
+-- | Convert a permutation to cycle notation
 toCycles :: Permutation -> [[Int]]
 toCycles = map (\(i:is) -> i : reverse is ) . toInverseCycles
 
--- Convert a permutation to cycle notation for its inverse
+-- | Convert a permutation to cycle notation for its inverse
 toInverseCycles :: Permutation -> [[Int]]
 toInverseCycles = map (\(i,is) -> i:is )
                 . M.toList
@@ -179,11 +174,11 @@ buildCycles (i, j) xss = case M.lookup j xss of
 
 
 
--- Given a permutation p and an index i, returns the index that i moves to under p
+-- | Given a permutation p and an index i, returns the index that i moves to under p
 imageOfIndex :: Permutation -> Int -> Int
 imageOfIndex p n = foldl (flip swap) n (toTranspositions p)
 
--- Given a permutation p and an index i, returns the index that moves to i under p
+-- | Given a permutation p and an index i, returns the index that moves to i under p
 preimageOfIndex :: Permutation -> Int -> Int
 preimageOfIndex p n = foldr swap n (toTranspositions p)
 
@@ -194,6 +189,11 @@ swap (i, j) n
   | otherwise = n
 
 
+fromTranspositions :: [(Int, Int)] -> Permutation
+fromTranspositions = Permutation . foldr (\(i,j) n -> (n + toInteger j) * toInteger (i-1) ) 0
+
+fromReverseTranspositions :: [(Int,Int)] -> Permutation
+fromReverseTranspositions = Permutation . foldl (\n (i,j) -> (n + toInteger j) * toInteger (i-1) ) 0
 
 -- This function assumes that i > j, k > l, k is strictly descending, and (i 0) represents
 -- swapping i with nothing, or the identity permutation.
